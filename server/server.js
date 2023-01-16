0b6f15c31eb34683248caed0d0adac89f66b1419
hewtungyuen <hewtungyuen@gmail.com>
2023-01-16 23:40:35 +0800

    add mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
+const { default: mongoose } = require("mongoose")
 const expensesRoute = require("./routes/expensesRoute")
 
 const app = express()
 
 app.use("/expenses", expensesRoute)
 
-app.listen(process.env.PORT, () => {
-    console.log("hello world")
-})
+
+
+mongoose.connect(
+    process.env.ATLAS_URI
+    ).then(
+        () => {
+            app.listen(process.env.PORT, () => {
+                console.log(`server connected at port ${process.env.PORT}`)
+            })
+        }
+    ).catch(
+        (err) => {
+            console.log(err)
+        }
+    )
