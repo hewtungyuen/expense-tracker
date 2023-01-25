fb436e7c7e489b039d64deaab300e6d77d2710d3
hewtungyuen <hewtungyuen@gmail.com>
2023-01-25 19:15:54 +0800

    add cancel command

--- telegram-client/telegram-client.js
+++ telegram-client/telegram-client.js
@@ -3,6 +3,7 @@ const { Telegraf } = require('telegraf');
 
 const { 
     initialiseBot,
+    displayMonthTotal
 } = require("./features/start")
 
 const {
@@ -11,16 +12,15 @@ const {
 
 const {
     confirmation,
-    deletePreviousExpense
 } = require("./features/deleteExpense")
 
 const { textHandler } = require('./states/stateHandler')
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command('/start', initialiseBot)
+bot.command('/cancel', displayMonthTotal)
 bot.hears('add expense', addNewExpense)
 bot.hears('delete expense', confirmation)
 bot.on('text', textHandler) 
-bot.hears('cancel')
 
 bot.launch()
