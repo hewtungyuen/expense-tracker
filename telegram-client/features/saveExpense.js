a0199a91b0549dd05c3dcf41de27169701b9014b
hewtungyuen <hewtungyuen@gmail.com>
2023-02-11 13:25:45 +0800

    refactor

--- telegram-client/features/saveExpense.js
+++ telegram-client/features/saveExpense.js
@@ -3,62 +3,51 @@ const state = require('../states/stateEnum')
 const { renderKeyboard } = require('./start')
 
 const saveExpenseToDatabase = async (ctx) => {
+    date = new Date()
+    const expense = {
+        "year" : date.getFullYear(),
+        "month" : date.getMonth() + 1,
+        "day" : date.getDate(),
+    }
+
     const telegramId = ctx.message.chat.username
     const description = await api.get(`/users/${telegramId}/expenseDescription`).then(value => value.data)
     const category = await api.get(`/users/${telegramId}/expenseCategory`).then(value => value.data)
+
+    expense.telegramId = telegramId
+    expense.expenseDescription = description
+    expense.expenseCategory = category
+
     const overseasMode = await api.get(`/users/${telegramId}/overseasMode`).then(value => value.data)
 
     if (overseasMode) {
         const expenseAmountOverseas = await api.get(`/users/${telegramId}/expenseAmountOverseas`).then(value => value.data)
         const tripName = await api.get(`/users/${telegramId}/tripName`).then(value => value.data)
         
+        expense.tripName = tripName
+
         if (expenseAmountOverseas != 0) {
+
             const exchangeRate = await api.get(`/users/${telegramId}/exchangeRate`).then(value => value.data)
-            await saveExpenseInOverseasCurrency(telegramId, expenseAmountOverseas, description, category, tripName, exchangeRate)
+            expense.exchangeRate = exchangeRate
+            expense.expenseAmountOverseas = expenseAmountOverseas
+
         } else {
             const expenseAmountSgd = await api.get(`/users/${telegramId}/expenseAmountSgd`).then(value => value.data)
-            await saveExpenseInSgd(telegramId, expenseAmountSgd, description, category, tripName)
+            expense.expenseAmountSgd = expenseAmountSgd
         }
 
     } else {
         const expenseAmountSgd = await api.get(`/users/${telegramId}/expenseAmountSgd`).then(value => value.data)
-        await saveExpenseInSgd(telegramId, expenseAmountSgd, description, category)
+        expense.expenseAmountSgd = expenseAmountSgd
+
     }
+
+    await api.post('/expenses', expense)
 
     await api.patch(`/users/${telegramId}`, {currentState: state.START})
     ctx.reply(`Added expense: ${description}`)
     renderKeyboard(ctx)
-}
-
-const saveExpenseInSgd = async (telegramId, amount, description, category, tripName='None') => {
-    if (tripName == 'None') {
-        await api.post('/expenses', {
-            "telegramId" : telegramId, 
-            "expenseAmountSgd" : amount,
-            "expenseDescription" : description,
-            "expenseCategory" : category 
-        })
-    } else {
-        await api.post('/expenses', {
-            "telegramId" : telegramId, 
-            "expenseAmountSgd" : amount,
-            "expenseDescription" : description,
-            "expenseCategory" : category,
-            "tripName" : tripName
-        })
-    }
-}
-
-const saveExpenseInOverseasCurrency = async (telegramId, amount, description, category, tripName, exchangeRate) => {
-    
-    await api.post('/expenses', {
-        "telegramId" : telegramId, 
-        "expenseAmountOverseas" : amount,
-        "expenseDescription" : description,
-        "expenseCategory" : category,
-        "tripName" : tripName,
-        "exchangeRate" : exchangeRate
-    })
 }
 
 module.exports = {
