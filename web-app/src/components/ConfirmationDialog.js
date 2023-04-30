73853df943a4f3d1f4b7c3c5a4a5e92b87c95361
hewtungyuen <hewtungyuen@gmail.com>
2023-05-01 01:19:32 +0800

    implement delete expense

---
+++ web-app/src/components/ConfirmationDialog.js
@@ -0,0 +1,37 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { useState } from "react";
+
+export default function ConfirmationDialog({ callBack }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleCloseWithCallback = () => {
+    callBack();
+    handleClose();
+  };
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Delete</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Delete this expense?</DialogTitle>
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+          <Button onClick={handleCloseWithCallback}>Yes</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
--- web-app/src/components/ExpenseDialog.js
+++ web-app/src/components/ExpenseDialog.js
@@ -19,6 +19,7 @@ import React, { useContext } from "react";
 import dayjs from "dayjs";
 import api from "../axiosConfig";
 import MyContext from "./utils/reactContext";
+import ConfirmationDialog from "./ConfirmationDialog";
 
 export default function ExpenseDialog({ open, closeDialog, expenseDetails }) {
   const { reRender } = useContext(MyContext);
@@ -40,10 +41,7 @@ export default function ExpenseDialog({ open, closeDialog, expenseDetails }) {
     }));
   };
 
-  const handleClose = async () => {
-    const res = await api
-      .post(`expenses/${expenseDetails._id}`, formData)
-      .then((x) => x.data);
+  const handleClose = () => {
     setFormData({
       expenseCategory: expenseDetails.expenseCategory,
       expenseDescription: expenseDetails.expenseDescription,
@@ -53,76 +51,91 @@ export default function ExpenseDialog({ open, closeDialog, expenseDetails }) {
       expenseAmountSgd: expenseDetails.expenseAmountSgd,
     });
     closeDialog();
+  };
+
+  const handleUpdate = async () => {
+    const res = await api
+      .post(`expenses/${expenseDetails._id}`, formData)
+      .then((x) => x.data);
+    handleClose();
+    reRender();
+  };
+
+  const handleDelete = async () => {
+    await api.delete(`expenses/${expenseDetails._id}`);
+    handleClose();
     reRender();
   };
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Edit expense details</DialogTitle>
-      <DialogContent>
-        <Stack spacing={2} padding={2}>
-          <TextField
-            name="expenseDescription"
-            label="Description"
-            fullWidth
-            defaultValue={expenseDetails.expenseDescription}
-            onChange={handleInputChange}
-          />
-
-          <FormControl fullWidth>
-            <InputLabel>Category</InputLabel>
-            <Select
-              name="expenseCategory"
-              defaultValue={expenseDetails.expenseCategory}
-              label="Category"
-              onChange={handleInputChange}
-            >
-              <MenuItem value={"Food"}>Food</MenuItem>
-              <MenuItem value={"Leisure"}>Leisure</MenuItem>
-              <MenuItem value={"Transport"}>Transport</MenuItem>
-              <MenuItem value={"Shopping"}>Shopping</MenuItem>
-            </Select>
-          </FormControl>
-
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={["DatePicker"]}>
-              <DatePicker
-                label="Date"
-                defaultValue={dayjs(expenseDetails.date).subtract(1, 'day')}
-                onChange={(newValue) =>
-                  handleInputChange({
-                    target: { name: "date", value: newValue.add(1, 'day') },
-                  })
-                }
-              />
-            </DemoContainer>
-          </LocalizationProvider>
-
-          <TextField
-            label="Amount (SGD)"
-            name="expenseAmountSgd"
-            fullWidth
-            defaultValue={expenseDetails.expenseAmountSgd}
-            onChange={handleInputChange}
-          />
-
-          {expenseDetails.expenseAmountOverseas > 0 && (
+    <>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit expense details</DialogTitle>
+        <DialogContent>
+          <Stack spacing={2} padding={2}>
             <TextField
-              name="expenseAmountOverseas"
-              label="Amount (Overseas)"
+              name="expenseDescription"
+              label="Description"
               fullWidth
-              defaultValue={expenseDetails.expenseAmountOverseas}
+              defaultValue={expenseDetails.expenseDescription}
               onChange={handleInputChange}
             />
-          )}
-        </Stack>
-      </DialogContent>
 
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleClose}>Delete</Button>
-        <Button onClick={handleClose}>Confirm</Button>
-      </DialogActions>
-    </Dialog>
+            <FormControl fullWidth>
+              <InputLabel>Category</InputLabel>
+              <Select
+                name="expenseCategory"
+                defaultValue={expenseDetails.expenseCategory}
+                label="Category"
+                onChange={handleInputChange}
+              >
+                <MenuItem value={"Food"}>Food</MenuItem>
+                <MenuItem value={"Leisure"}>Leisure</MenuItem>
+                <MenuItem value={"Transport"}>Transport</MenuItem>
+                <MenuItem value={"Shopping"}>Shopping</MenuItem>
+              </Select>
+            </FormControl>
+
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={["DatePicker"]}>
+                <DatePicker
+                  label="Date"
+                  defaultValue={dayjs(expenseDetails.date).subtract(1, "day")}
+                  onChange={(newValue) =>
+                    handleInputChange({
+                      target: { name: "date", value: newValue.add(1, "day") },
+                    })
+                  }
+                />
+              </DemoContainer>
+            </LocalizationProvider>
+
+            <TextField
+              label="Amount (SGD)"
+              name="expenseAmountSgd"
+              fullWidth
+              defaultValue={expenseDetails.expenseAmountSgd}
+              onChange={handleInputChange}
+            />
+
+            {expenseDetails.expenseAmountOverseas > 0 && (
+              <TextField
+                name="expenseAmountOverseas"
+                label="Amount (Overseas)"
+                fullWidth
+                defaultValue={expenseDetails.expenseAmountOverseas}
+                onChange={handleInputChange}
+              />
+            )}
+          </Stack>
+        </DialogContent>
+
+        <DialogActions>
+          <ConfirmationDialog callBack={handleDelete} />
+          <Button onClick={handleUpdate}>Update</Button>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
