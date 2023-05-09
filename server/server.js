e61a8a28a12684890106223d7b466239241ba9b3
hewtungyuen <hewtungyuen@gmail.com>
2023-05-09 18:53:34 +0800

    combine bot and backend server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,25 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { Telegraf } = require("telegraf");
+const { initialiseBot } = require("./telegram-client/features/start");
+const {
+  switchToOverseasMode,
+} = require("./telegram-client/features/localMode");
+const { addNewExpense } = require("./telegram-client/features/addExpense");
+const {
+  deleteConfirmation,
+} = require("./telegram-client/features/deleteExpense");
+const {
+  switchToLocalModeConfirmation,
+} = require("./telegram-client/features/overseasMode");
+const {
+  enterExchangeRate,
+  enterOverseasCurrency,
+} = require("./telegram-client/features/currencyExchange");
+
+const { textHandler } = require("./telegram-client/states/textHandler");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // express + middleware
 const app = express();
@@ -20,6 +39,10 @@ app.get("/", (req, res) => {
   res.json("Working");
 });
 
+app.post("/bot", (req, res) => {
+  bot.handleUpdate(req.body, res);
+});
+
 // starting the app
 mongoose
   .connect(process.env.ATLAS_URI, {
@@ -33,3 +56,34 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// commands
+bot.command("/start", initialiseBot);
+bot.command("/cancel", initialiseBot);
+
+// local mode
+bot.hears("Add expense", addNewExpense);
+bot.hears("Delete expense", deleteConfirmation);
+bot.hears("Overseas mode", switchToOverseasMode);
+
+// overseas mode
+bot.hears("Local mode", switchToLocalModeConfirmation);
+bot.hears("Set exchange rate", enterExchangeRate);
+bot.hears("Change currency", enterOverseasCurrency);
+
+// text handler
+bot.on("text", textHandler);
+
+if (process.env.ENV === "PROD") {
+  bot.telegram.setWebhook(process.env.ENDPOINT_PROD + "/bot");
+
+  bot.launch({
+    webhook: {
+      domain: process.env.ENDPOINT_PROD,
+      port: 4000,
+      hookPath: "/bot",
+    },
+  });
+} else {
+  bot.launch();
+}
